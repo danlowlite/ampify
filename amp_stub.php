6e5e7afe33a3c0c4aeaa889df99e5578c93745ad
drm <danlowlite@gmail.com>
2016-05-30 00:22:10 -0500

    Update amp_stub.php

--- amp_stub.php
+++ amp_stub.php
@@ -1,6 +1,5 @@
 <?php 
 	session_start();
-//call with the filename included as the project, but we'll start with a known one.
 	$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 	$fileToAmp = substr(basename(__FILE__), 4);
 	include_once 'ampify.php';
